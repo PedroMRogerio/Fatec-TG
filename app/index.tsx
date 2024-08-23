1f574da1f12406911218b1b14dc2b14695fbfb7f
Pedro Moraes <pm-rogerio@hotmail.com>
2024-08-23 11:57:56 -0300

    Protótipo da tela de login e instalação de novos componentes

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
-import { StatusBar } from 'expo-status-bar'
+//import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, View } from 'react-native'
 import { router } from 'expo-router'
 import { Button, ButtonGroup, ButtonIcon, ButtonSpinner, ButtonText } from '@/components/ui/button'
-import { Text } from '@/components/ui/text'
+//import { Text } from '@/components/ui/text'
 import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input'
+import { Center } from '@/components/ui/center'
+import { Box } from '@/components/ui/box'
+import { VStack } from '@/components/ui/vstack'
+import { Heading } from '@/components/ui/heading'
+
 
 export default function Login() {
 
@@ -13,19 +18,36 @@ export default function Login() {
 
     return (
         <View style={styles.container}>
-            <Text>Login</Text>
-            <Input variant="outline" size="md" isDisabled={false} isInvalid={false} isReadOnly={false} >
-                <InputField placeholder="Digite seu login" type="text" />
-            </Input>
-            <Input variant="outline" size="md" isDisabled={false} isInvalid={false} isReadOnly={false}>
-                <InputField placeholder="Digite seua senha" type="password" />
-            </Input>
-            <ButtonGroup>
-                <Button size='sm' variant='solid' onPress={handleLogin}>
-                    <ButtonText>Login</ButtonText>
-                </Button>
-            </ButtonGroup>
-            <StatusBar style="auto" />
+            <Center>
+                <Box className='p-5 max-w-96 border border-background-300 rounded-lg'>
+                    <VStack className='pb-4' space='xs'>
+                        <Heading className='leading-[30px]'>
+                            Login
+                        </Heading>
+                    </VStack>
+                    <VStack space='xl' className='py-2'>
+                        <Input>
+                            <InputField
+                                className='py-2'
+                                placeholder="Login"
+                            />
+                        </Input>
+                        <Input>
+                            <InputField
+                                className='py-2'
+                                placeholder="Senha"
+                            />
+                        </Input>
+                    </VStack>
+                    <VStack space='lg' className='pt-4'>
+                        <Button size='sm'>
+                            <ButtonText>
+                                Submit
+                            </ButtonText>
+                        </Button>
+                    </VStack>
+                </Box>
+            </Center>
         </View>
     )
 }
