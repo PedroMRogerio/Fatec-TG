436cdbc77162a923ea6f8e41e7974ac68c013d78
Pedro Moraes <pm-rogerio@hotmail.com>
2024-08-27 11:03:28 -0300

    Atualização na tela de Login

--- app/index.tsx
+++ app/index.tsx
@@ -1,50 +1,55 @@
-//import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, View } from 'react-native'
-import { router } from 'expo-router'
-import { Button, ButtonIcon, ButtonText } from '@/components/ui/button'
-//import { Text } from '@/components/ui/text'
-import { Input, InputField } from '@/components/ui/input'
-//import { Center } from '@/components/ui/center'
-import { Box } from '@/components/ui/box'
-//import { VStack } from '@/components/ui/vstack'
-import { Heading } from '@/components/ui/heading'
-import { ArrowRightIcon, Icon } from '@/components/ui/icon'
-//import { Link } from '@/components/ui/link'
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { router } from 'expo-router';
+import { Button, ButtonText } from '@/components/ui/button';
+import { Input, InputField, InputIcon, InputSlot } from '@/components/ui/input';
+import { Box } from '@/components/ui/box';
+import { Heading } from '@/components/ui/heading';
+import { EyeIcon, EyeOffIcon } from 'lucide-react-native';
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handlePasswordVisibility = () => {
+        setShowPassword(prevState => !prevState);
+    };
+
     const handleLogin = () => {
-        router.replace('home')
-    }
+        router.replace('home');
+    };
 
     return (
         <View style={styles.container}>
-            <Heading>Login</Heading>
-            <Input>
-                <InputField className='py-2' placeholder="Insira o seu login" />
-            </Input>
-            <Input>
-                <InputField className='py-2' placeholder="Insira a sua senha" />
-            </Input>
-            <Box>
-            <Button size='sm' variant="solid" onPress={handleLogin}>
-                <ButtonText>Login</ButtonText>
-                    <Icon as={ArrowRightIcon} />
-            </Button>
+            <Box className='p-4 border rounded-lg border-outline-300'>
+                <Heading className='text-typography-900 leading-3'>Login</Heading>
+                <Box className='mt-4'>
+                    <Box className='mb-2'>
+                        <Input>
+                            <InputField placeholder="Insira o seu Login" className='py-2' type="text" />
+                        </Input>
+                    </Box>
+                    <Box className='mb-2'>
+                        <Input>
+                            <InputField placeholder="Insira a sua Senha" className='py-2' type={showPassword ? 'text' : 'password'} />
+                            <InputSlot className='pr-3' onPress={handlePasswordVisibility}>
+                                <InputIcon as={showPassword ? EyeIcon : EyeOffIcon} className='text-darkBlue-500' />
+                            </InputSlot>
+                        </Input>
+                    </Box>
+                    <Button className='ml-auto' onPress={handleLogin}>
+                        <ButtonText className='text-typography-0'>Login</ButtonText>
+                    </Button>
+                </Box>
             </Box>
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    /*box: {
-        width: '80%', // Ajusta a largura do Box para 80% da tela
-        alignItems: 'center', // Centraliza os itens dentro do Box
-    },*/
-})
+});
