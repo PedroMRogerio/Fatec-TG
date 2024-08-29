8f782b55ba5e33a03e1ab635397c8080847fa6a0
Caio Assis <caio.ab.assis@gmail.com>
2024-08-29 17:03:20 -0300

    Adiciona style em Home

--- app/home.tsx
+++ app/home.tsx
@@ -1,5 +1,11 @@
+import { View, Text } from 'react-native'
+import {styles} from './index'
 
 export default function Home() {
-
+    return(
+        <View style={styles.view}>
+            <Text style={styles.texto}>Teste Home</Text>
+        </View>
+    )
 
 }
