d49f8f914a61845408c89f7ec56a131aa33d8876
Caio Assis <caio.ab.assis@gmail.com>
2024-09-16 18:13:54 -0300

    Cria interfaces iniciais

---
+++ components/interfaces/user-client.tsx
@@ -0,0 +1,6 @@
+export interface UserClient {
+    name: string
+    email: string
+    password: string
+    cpf: string
+}
---
+++ components/interfaces/user-provider.tsx
@@ -0,0 +1,7 @@
+export interface UserProvider {
+    name: string
+    email: string
+    password: string
+    cpf: string
+    cnh: string
+}
