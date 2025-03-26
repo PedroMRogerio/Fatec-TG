a3059471cfdc4caa3d78c6117dcf0d3b980d48a5
Caio Assis <caio.ab.assis@gmail.com>
2025-03-26 10:23:39 -0300

    exclui teste-mapa-2

--- app/home.tsx
+++ app/home.tsx
@@ -49,11 +49,6 @@ export default function Home() {
                         <Text style={styles.textoBotoes}>Teste Mapa</Text>
                     </Pressable>
                 </Link>
-                <Link href="/teste-mapa-2" asChild>
-                    <Pressable style={styles.botoes}>
-                        <Text style={styles.textoBotoes}>Teste Mapa 2</Text>
-                    </Pressable>
-                </Link>
             </View >
         </View >
     )
