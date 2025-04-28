b952bd42f4b42cbb8452bf1310f3841ce85cb7ad
Caio Assis <caio.ab.assis@gmail.com>
2025-04-27 22:12:37 -0300

    problema nao identificado (index.tsx)

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
-    plugins: [['module:react-native-dotenv']],
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'expo-router/babel',
+      ['module:react-native-dotenv'],
+    ]
   };
 };
