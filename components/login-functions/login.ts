c13708da5d37a0721ddd86918a2fa88230f3c05c
Caio Assis <caio.ab.assis@gmail.com>
2025-05-18 09:34:21 -0300

    Desiste de AsyncStorage

--- components/login-functions/login.ts
+++ components/login-functions/login.ts
@@ -1,11 +1,17 @@
 import { signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "@/helpers/firebaseConfig"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 export const signIn = async (email: string, password: string) => {
   try {
-    await signInWithEmailAndPassword(auth, email, password)
-    //console.log("Usuário logado com sucesso")
-    //console.log(userId)
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    const user = userCredential.user;
+
+    await AsyncStorage.setItem('@user', JSON.stringify({
+      uid: user.uid,
+      email: user.email
+    }));
+
   } catch (error: any) {
     console.error("Erro ao logar:", error.message)
     throw new Error(error.message)
