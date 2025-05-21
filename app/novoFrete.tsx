68aa1d9465e7dd91928f487be3fa8a25fd1582b7
Caio Assis <caio.ab.assis@gmail.com>
2025-05-21 10:04:18 -0300

    Altera tela novoFrete

--- app/novoFrete.tsx
+++ app/novoFrete.tsx
@@ -1,27 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { View, Text } from "react-native";
-import UserCliQuery from "@/components/firestore-query/userCli";
+import { View } from "react-native";
 
-export default function NovoFrete() {
-  const [usersText, setUsersText] = useState<string>("Carregando...");
+export default function NovoFrete(){
 
-  async function Teste() {
-    try {
-      const users = await UserCliQuery.getUser('s'); // Pode ser qualquer array
-      setUsersText(JSON.stringify(users, null, 2)); // Exibe o array completo formatado
-    } catch (error) {
-      setUsersText("Erro ao carregar usuários");
-      console.error(error);
-    }
-  }
+    return (
+        <View>
 
-  useEffect(() => {
-    Teste();
-  }, []);
-
-  return (
-    <View>
-      <Text>{usersText}</Text>
-    </View>
-  );
+        </View>
+    )
 }
