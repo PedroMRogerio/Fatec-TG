639cbe909311377886c7939949b5ccf7ce4a9b77
Caio Assis <caio.ab.assis@gmail.com>
2025-05-25 09:49:03 -0300

    altera navegacao

--- app/content/home.tsx
+++ app/content/home.tsx
@@ -1,15 +1,9 @@
-import { Link } from "expo-router";
-import { View, StyleSheet, Text, Pressable, ScrollView, Image, Dimensions } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Dimensions } from "react-native";
 import { useUser } from "@/contexts/userContext";
 import FreteCardList from "@/components/frete/freteCard";
 import ProvCardList from "@/components/frete/provCard";
 import { useFocusEffect } from "@react-navigation/native";
 import React, { useState, useCallback } from "react";
-
-import userButton from "@/assets/images/userButton.png";
-import newFreteButton from "@/assets/images/newFreteButton.png";
-import searchButton from "@/assets/images/searchButton.png"
-import configButton from "@/assets/images/configButton.png";
 
 const { width } = Dimensions.get("window");
 
@@ -43,32 +37,7 @@ export default function Home() {
                 <View style={styles.separador2}></View>
             </ScrollView>
 
-            <View style={styles.view2}>
-                <Link href="/content/usuario" asChild>
-                    <Pressable style={styles.botoes}>
-                        <Image source={userButton} style={styles.icon} />
-                    </Pressable>
-                </Link>
-                {user?.uType === 'prov' && (
-                <Link href="/content/frete-buscar" asChild>
-                    <Pressable style={styles.botoes2}>
-                        <Image source={searchButton} style={styles.icon} />
-                    </Pressable>
-                </Link>
-                )}
-                {user?.uType === 'cli' && (
-                <Link href="/content/novoFrete" asChild>
-                    <Pressable style={styles.botoes2}>
-                        <Image source={newFreteButton} style={styles.icon} />
-                    </Pressable>
-                </Link>
-                )}
-                <Link href="/content/config" asChild>
-                    <Pressable style={styles.botoes}>
-                        <Image source={configButton} style={styles.icon} />
-                    </Pressable>
-                </Link>
-            </View>
+            
         </View>
     );
 }
@@ -98,42 +67,5 @@ export const styles = StyleSheet.create({
     },
     separador2: {
         height: 20,
-    },
-    view2: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        height: 80,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        borderTopWidth: 0.5,
-        borderTopColor: '#808080',
-        backgroundColor: '#fff',
-        paddingBottom: 10,
-    },
-    botoes: {
-        //borderWidth: 1,
-        width: width*0.12,
-        height: width*0.12,
-        //borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
-    },
-    botoes2: {
-        //borderWidth: 1,
-        width: width*0.15,
-        height: width*0.18,
-        //borderRadius: 50,
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
-    },
-    icon: {
-        width: '100%',
-        height: '100%',
-        resizeMode: 'contain',
     },
 });
